6cc089425e09b578c3dd28cf74651de777bc9a10
hua洋 <46264948+MHYCDH@users.noreply.github.com>
2019-07-02 13:23:35 +0800

    Update titleqipian.js

--- js/titleqipian.js
+++ js/titleqipian.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = 'Error 404 not found';
+         document.title = 'Σ((( つ•̀ω•́)つ赶紧躲起来了';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '嗯?~' + OriginTitle;
+         document.title = '(๑•́ ₃ •̀๑)被发现了' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
