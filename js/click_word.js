bb94581b66ee3b2a631acd14bd7b2f22e80b1144
hua洋 <46264948+MHYCDH@users.noreply.github.com>
2019-07-18 19:25:59 +0800

    Update click_word.js

--- js/click_word.js
+++ js/click_word.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("(๑• . •๑)", "⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄", "o(*////▽////*)q", "Σ( ° △ °|||)", "Σ(っ °Д °;)っ", "ヽ(*。>Д<)o゜", "=͟͟͞͞( •̀д•́)", "─=≡Σ((( つ•̀ω•́)つ", "(╯°Д°)╯︵ ┻━┻", "(￣ε(#￣)☆╰╮o(￣皿￣///)", "( >﹏<。)");
+        ("(๑• . •๑)", "⁄(⁄ ⁄•⁄ω⁄•⁄ ⁄)⁄", "Σ( ° △ °|||)", "Σ(っ °Д °;)っ", "ヽ(*。>Д<)o゜", "=͟͟͞͞( •̀д•́)", "≡Σ((( つ•̀ω•́)つ", "(╯°Д°)╯︵ ┻━┻", "(￣ε(#￣)☆╰╮o(￣皿￣///)", "( >﹏<。)");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
@@ -13,7 +13,7 @@ jQuery(document).ready(function($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#FF0000"
+            "color": "#000080"
         });
         $("body").append($i);
         $i.animate({
