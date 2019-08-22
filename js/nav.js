7a72e78a57e55f333f494d340bd0314ca190cf92
AhogeK <ahogek@gmail.com>
2019-08-22 19:55:20 +0800

    bugs update

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
-$(function() {
+$(document).ready(function() {
     if ($(".navbar-brand").css("color") == "#fff") {
         $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
         $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
     }
-})
+});
