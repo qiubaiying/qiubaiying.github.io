8ab37dd7b7813cc688df6d74e43dfa858c7b98c2
AhogeK <ahogek@gmail.com>
2019-08-22 19:52:27 +0800

    style update

---
+++ js/nav.js
@@ -0,0 +1,6 @@
+$(function() {
+    if ($(".navbar-brand").css("color") == "#fff") {
+        $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
+        $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
+    }
+})
