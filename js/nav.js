b3c95ce5e5c5445a6a9920d322a6337951f50206
AhogeK <ahogek@gmail.com>
2019-08-22 20:31:47 +0800

    bugs update

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
-    if ($(".navbar-brand").css("color") == "#fff") {
-        $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
-        $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
+// $(document).ready(function() {
+//     if ($(".navbar-brand").css("color") == "#fff") {
+//         $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
+//         $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
+//     }
+// });
+
+Window.onload=function(){
+    if(document.getElementsByClassName("navbar-brand").style.color == "#fff") {
+        document.getElementsByClassName("navbar-brand").style['text-shadow'] = "black 0.1em 0.1em 0.2em";
+        document.getElementsByClassName(".nav").getElementsByTagName("li").style['text-shadow'] = "black 0.1em 0.1em 0.2em";
     }
-});
+}
