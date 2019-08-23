443a84cb7b8df11dac96741e5241e877a0e8b5e7
AhogeK <ahogek@gmail.com>
2019-08-23 12:33:07 +0800

    修复bug

--- js/nav.js
+++ js/nav.js
@@ -4,14 +4,13 @@ $(document).ready(function() {
     console.log(str);
     $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
     $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-});
-
-$(".navbar-brand").change(function (e) { 
-    if ($(".navbar-brand").css("color") + "" != "rgb(255, 255, 255)") {
-        $(".navbar-brand").css("text-shadow", "");
-        $(".nav").children("li").css("text-shadow", "");
-    } else {
-        $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
-        $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-    }
+    $(".navbar-brand").change(function (e) { 
+        if ($(".navbar-brand").css("color") + "" != "rgb(255, 255, 255)") {
+            $(".navbar-brand").css("text-shadow", "");
+            $(".nav").children("li").css("text-shadow", "");
+        } else {
+            $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
+            $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
+        }
+    });
 });
