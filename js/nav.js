e19d5e1fb44da82c9a8837f98338eceda8dc81fc
AhogeK <ahogek@gmail.com>
2019-08-23 13:08:51 +0800

    bug 修复

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("article").css("overflow", "hidden");
     var str = $(".navbar-brand").css("color") + "";
     console.log(str);
-    if(str === "rgb(255, 255, 255)"){
-        $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
-        $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-    }
+
+    $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
+    $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
+
+    $(".navbar-brand").change(function (e) {
+        if (str === "rgb(255, 255, 255)") {
+            $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
+            $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
+        } else {
+            $(".navbar-brand").css("text-shadow", "");
+            $(".nav").children("li").css("text-shadow", "");
+        }
+    });
 });
