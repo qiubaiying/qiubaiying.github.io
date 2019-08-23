d99eb8ebc5079dc4d9483ca856b613094d5ae601
AhogeK <ahogek@gmail.com>
2019-08-23 13:06:17 +0800

    test

--- js/nav.js
+++ js/nav.js
@@ -2,21 +2,8 @@ $(document).ready(function() {
     $("article").css("overflow", "hidden");
     var str = $(".navbar-brand").css("color") + "";
     console.log(str);
-    $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
-    $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-    // $("nav").change(function (e) { 
-    //     if ($(".navbar-brand").css("color") + "" != "rgb(255, 255, 255)") {
-    //         $(".navbar-brand").css("text-shadow", "");
-    //         $(".nav").children("li").css("text-shadow", "");
-    //     } else {
-    //         $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
-    //         $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-    //     }
-    // });
-    $("nav").change(function (e) { 
-        $('.navbar-custom').css("text-shadow","black 0.1em 0.1em 0.2em");
-        $('.navbar-custom').children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-        $('.is-visible').css("text-shadow","");
-        $('.is-visible').children("li").css("text-shadow","");
-    });
+    if(str === "rgb(255, 255, 255)"){
+        $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
+        $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
+    }
 });
