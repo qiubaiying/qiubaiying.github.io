560227b2d3ff44698fa64f86f3e109af520d9f09
AhogeK <ahogek@gmail.com>
2019-08-23 11:44:19 +0800

    style test

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    // console.log($(".navbar-brand").css("color"));
-    // $(".navbar-brand").change(function (e) { 
-        // if ($(".navbar-brand").css("color") + "" == "rgb(255, 255, 255)") {
+    $(".navbar-brand").css("color", "white");
+     $(".navbar-brand").change(function (e) { 
+         if ($(".navbar-brand").css("color") + "" == "white") {
             $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
             $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-        // } else {
-            // $(".navbar-brand").css("");
-            // $(".nav").children("li").css("");
-        // }
-    // });
+        } else {
+            $(".navbar-brand").css("");
+            $(".nav").children("li").css("");
+        }
+    });
 });
