2a0bb30f47ecfd35d4103412c6e5e2f46296ead9
AhogeK <ahogek@gmail.com>
2019-08-23 13:35:03 +0800

    bug修复

--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,11 @@
-// $(document).ready(function () {
-//     $("article").css("overflow", "hidden");
+$(document).ready(function () {
+    $("article").css("overflow", "hidden");
 
-//     $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
-//     $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
-//     $("nav").on()
-//     $("nav").change(function (e) {
-//         console.log($(".navbar-brand").css("color") + "");
-//         if ($(".navbar-brand").css("color") + "" === "rgb(255, 255, 255)") {
-//             $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
-//             $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
-//         } else {
-//             $(".navbar-brand").css("text-shadow", "");
-//             $(".nav").children("li").css("text-shadow", "");
-//         }
-//     });
-// });
+    $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
+    $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
+});
 $(window).scroll(function(){
-    if ($(window).scrollTop() == 0) {
+    if ($(window).scrollTop() <= 60) {
         $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
         $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
     } else {
