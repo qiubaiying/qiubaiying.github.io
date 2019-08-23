bf0e67c71a93c14e7a5b73d42713e69dccf82cb7
AhogeK <ahogek@gmail.com>
2019-08-23 11:25:54 +0800

    fixe bugs

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     console.log($(".navbar-brand").css("color"));
     $(".navbar-brand").change(function (e) { 
-        if ($(".navbar-brand").css("color") == "rgb(255, 255, 255)") {
+        if ($(".navbar-brand").css("color") + "" == "rgb(255, 255, 255)") {
             $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
             $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
         } else {
