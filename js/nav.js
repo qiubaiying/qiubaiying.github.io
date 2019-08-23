a9449899a8e678cc5f07f88c1e47eb6a027f5256
AhogeK <ahogek@gmail.com>
2019-08-23 11:07:39 +0800

    nav update

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-    if ($(".navbar-brand").css("color") == "#fff") {
-        $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
-        $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
-    }
+    $(".navbar-brand").change(function (e) { 
+        if ($(".navbar-brand").css("color") == "#fff") {
+            $(".navbar-brand").css("text-shadow","black 0.1em 0.1em 0.2em");
+            $(".nav").children("li").css("text-shadow","black 0.1em 0.1em 0.2em");
+        } else {
+            $(".navbar-brand").css("");
+            $(".nav").children("li").css("");
+        }
+    });
 });
