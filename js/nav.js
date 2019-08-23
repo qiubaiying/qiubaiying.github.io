532163d9238e85ca830ec2846be11f18d3daf691
AhogeK <ahogek@gmail.com>
2019-08-23 14:00:55 +0800

    完善手机适配

--- js/nav.js
+++ js/nav.js
@@ -5,11 +5,21 @@ $(document).ready(function () {
     $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
 });
 $(window).scroll(function(){
-    if ($(window).scrollTop() == 0) {
-        $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
-        $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
+    if (screen && screen.width > 480) {
+        if ($(window).scrollTop() == 0) {
+            $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
+            $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
+        } else {
+            $(".navbar-brand").css("text-shadow", "");
+            $(".nav").children("li").css("text-shadow", "");
+        }
     } else {
-        $(".navbar-brand").css("text-shadow", "");
-        $(".nav").children("li").css("text-shadow", "");
+        if ($(window).scrollTop() <= 60) {
+            $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
+            $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
+        } else {
+            $(".navbar-brand").css("text-shadow", "");
+            $(".nav").children("li").css("text-shadow", "");
+        }
     }
 });
