384a6f284be9466ca2d671f1fefc3cb8971cec73
AhogeK <ahogek@gmail.com>
2019-08-24 10:34:25 +0800

    手机端bug修复

--- js/nav.js
+++ js/nav.js
@@ -16,10 +16,8 @@ $(window).scroll(function(){
     } else {
         if ($(window).scrollTop() <= 60) {
             $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
-            $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
         } else {
             $(".navbar-brand").css("text-shadow", "");
-            $(".nav").children("li").css("text-shadow", "");
         }
     }
 });
