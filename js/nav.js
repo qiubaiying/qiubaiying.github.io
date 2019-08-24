59e4a3654a30513cf8c1caf81a8b6b5eaf9a6aa3
AhogeK <ahogek@gmail.com>
2019-08-24 10:37:02 +0800

    bug 修复

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
     $("article").css("overflow", "hidden");
-
-    $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
-    $(".nav").children("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
 });
 $(window).scroll(function(){
     if (screen && screen.width > 480) {
@@ -17,7 +14,7 @@ $(window).scroll(function(){
         if ($(window).scrollTop() <= 60) {
             $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
         } else {
-            $(".navbar-brand").css("text-shadow", "");
+            $(".navbar-brand").css("text-shadow", "")
         }
     }
 });
