65461398c8240878466bf06de17353cb670921c8
AhogeK <ahogek@gmail.com>
2019-08-24 10:42:54 +0800

    bug修复

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     $("article").css("overflow", "hidden");
+
+    $(".navbar-brand").css("text-shadow", "black 0.1em 0.1em 0.2em");
+    $("#huxblog_navbar").find("li").css("text-shadow", "black 0.1em 0.1em 0.2em");
 });
 $(window).scroll(function(){
     if (screen && screen.width > 480) {
