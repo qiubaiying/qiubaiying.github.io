7518828d7b46d4597c4e2b20d28455263fa7c8c0
Billy_Xie <66951589+xie96808@users.noreply.github.com>
2022-05-13 11:01:42 +0800

    Update Gitalk.js

--- Gitalk.js
+++ Gitalk.js
@@ -9,13 +9,13 @@
     <script type="text/javascript">
     var gitalk = new Gitalk({
     // gitalk的主要参数
-	clientID: `4dede977f9d18025b3b5`,
-	clientSecret: `993a40becb3f6e9bb2da6565ca2cbb9b486624f8`,
-	repo: `xie96808.github.io`,
-	owner: 'xie96808',
-	admin: ['xie96808'],
-	id: 'https://github.com/xie96808/xie96808.github.io',
-    
+	clientID: 4dede977f9d18025b3b5,
+	clientSecret: 993a40becb3f6e9bb2da6565ca2cbb9b486624f8,
+	repo: xie96808.github.io,
+	owner: xie96808,
+	admin: [xie96808],
+	id: xie96808.github.io/_posts,
+    	distractionFreeMode: false  // Facebook-like distraction free mode
     });
     gitalk.render('gitalk-container');
 </script>
